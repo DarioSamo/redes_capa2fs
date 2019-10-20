55e0dc8f6bb61abd894d5a417dbf8a3a77a1779b
Dario <dariosamo@gmail.com>
2019-10-20 11:31:28 -0700

    More tests

--- receiver.py
+++ receiver.py
@@ -1,19 +1,27 @@
-from rawsocketpy import RawSocket, to_str
 
-print("hola1")
-sock = RawSocket("ens33", 0xEEFA)
-print("hola2")
-packet = sock.recv()
-print("hola3")
-# The type of packet is RawPacket() which allows pretty printing and unmarshal the raw data.
+from rawsocketpy import RawRequestHandler, RawAsyncServerCallback
+import time
 
-# If you are using Python2, all data is encoded as unicode strings "\x01.." while Python3 uses bytearray.
+def callback(handler, server):
+    print("Testing")
+    handler.setup()
+    handler.handle()
+    handler.finish()
 
-print(packet) # Pretty print
-packet.dest # string "\xFF\xFF\xFF\xFF\xFF\xFF" or bytearray(b"\xFF\xFF\xFF\xFF\xFF\xFF")
-packet.src # string "\x12\x12\x12\x12\x12\x13" or bytearray(b"\x12\x12\x12\x12\x12\x13")
-packet.type # string "\xEE\xFA" or bytearray([b"\xEE\xFA"]
-packegt.data # string "some data" or bytearray(b"some data"]
+class LongTaskTest(RawRequestHandler):
+    def handle(self):
+        time.sleep(1)
+        print(self.packet)
 
-print(to_str(packet.dest)) # Human readable MAC: FF:FF:FF:FF:FF:FF
-print(to_str(packet.type, "")) # Human readable type: EEFA
+    def finish(self):
+        print("End")
+
+    def setup(self):
+        print("Begin") 
+
+def main():
+    rs = RawAsyncServerCallback("ens33", 0xEEFA, LongTaskTest, callback)
+    rs.spin()
+
+if __name__ == '__main__':
+    main()
