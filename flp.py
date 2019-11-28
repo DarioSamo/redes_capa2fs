ecde859651557fcdccedbe3753484dee2f4b4205
JPCard <44414304+JPCard@users.noreply.github.com>
2019-11-28 01:17:58 -0300

    bug fix en dirFromFilePathIsMounted(filePath)

--- flp.py
+++ flp.py
@@ -103,8 +103,9 @@ def handleGetfile(data, dest):
 def dirFromFilePathIsMounted(filePath):
     if(os.path.isdir(filePath)): #its a directory instead of a file
         return False
-    posDirEnd = filePath.rindex("/")
-    dirPath = filePath[:posDirEnd]
+    posDirEnd = filePath.rindex("/") 
+    dirPath = filePath[:(posDirEnd + 1)]
+    #+1 to transform from [0,n] scale to [1,n+1] scale expected in [:]
     
     return dirIsMounted(dirPath)
 
